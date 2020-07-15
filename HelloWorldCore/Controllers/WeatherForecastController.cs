a6c8a89670b283eb95a68602a03460db8613d561
Alex Occhipinti <mrocchipinti@Gmail.com>
2020-07-15 12:32:07 -0400

    [12345667] OMG Updates

--- HelloWorldCore/Controllers/WeatherForecastController.cs
+++ HelloWorldCore/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace HelloWorldCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "XXXXXXXXXXXXXXXXXXXXXX", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
